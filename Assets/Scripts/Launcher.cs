ae8864452426edb3a33684295903c5e7de94f133
woojiwon <woojiwon060317@gmail.com>
2023-11-05 21:54:35 +0900

    Create Other Menu

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using System.ComponentModel.Design;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
     void Start()
     {
         Debug.Log("Connecting to Master");
@@ -24,8 +28,36 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("Joined Lobby");
     }
 
-    void Update()
+    public void CreateRoom()     // room을 생성하고 자동으로 room에 들어감
     {
-        
+        if(string.IsNullOrEmpty(roomNameInputField.text))       // InputField가 null인지 아닌지를 비교 후 null이면 true를 반환
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);      // room의 이름을 인자로 받으면서 room생성
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()     // room에 들어갔으면 호출
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;     // 현재 들어온 room의 이름을 받아옴
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)       // room에 들어가는 것을 실패했을 때 호출
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();      // 현재 room을 나감
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()       // room을 떠나면(LeaveRoom()함수가 호출되면) 호출되는 함수
+    {
+        MenuManager.Instance.OpenMenu("title");
     }
 }
