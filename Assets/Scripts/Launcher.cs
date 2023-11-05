26b6bca75ab0e491b1713a59dff233925826ff43
woojiwon <woojiwon060317@gmail.com>
2023-11-06 01:17:54 +0900

    Create Find Menu

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     void Start()
     {
@@ -56,8 +66,35 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);      // info.Name(이름? ID?)로 room에 참가
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
     public override void OnLeftRoom()       // room을 떠나면(LeaveRoom()함수가 호출되면) 호출되는 함수
     {
         MenuManager.Instance.OpenMenu("title");
+    }
+
+    #region 아래 함수 설명
+    // room을 갱신하기 위해서 사용
+    // 실행되는 경우
+    // -로비에 접속 시
+    // -새로운 룸이 만들어질 경우
+    // -룸이 삭제되는 경우
+    // -룸의 IsOpen 값이 변화할 경우(아예 RoomInfo 내 데이터가 바뀌는 경우 전체일 수도 있습니다)
+    // -매개변수로 넘어오는 roomList는 "현재 존재하는 방 전부"가 아니라 "변동사항이 있는 방"만 넘어옴
+    #endregion
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach(Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
     }
 }
