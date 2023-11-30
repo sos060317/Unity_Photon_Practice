f33df90532770fed1c24258168bac232a67cccb0
WooJiWon <woojiwon060317@gmail.com>
2023-11-30 15:15:00 +0900

    Create BulletImpact

--- Assets/Scripts/SingleShotGun.cs
+++ Assets/Scripts/SingleShotGun.cs
@@ -7,6 +7,13 @@ public class SingleShotGun : Gun
 {
     [SerializeField] Camera cam;
 
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
     public override void Use()
     {
         Shoot();
@@ -19,12 +26,14 @@ public class SingleShotGun : Gun
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             hit.collider.gameObject.GetComponent<IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+            PV.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
         }
     }
 
     [PunRPC]
-    private void RPC_Shoot(Vector3 hitPosition)
+    private void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
     {
-
+        Instantiate(bulletImpactPrefab, hitPosition, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+        //Instantiate(bulletImpactPrefab, hitPosition, Quaternion.LookRotation(hitNormal) * bulletImpactPrefab.transform.rotation);
     }
 }
