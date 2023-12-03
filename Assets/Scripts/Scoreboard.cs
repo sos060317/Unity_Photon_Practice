2c3e49531bfa63099374393fe1dcd860b0b97386
WooJiWon <woojiwon060317@gmail.com>
2023-12-03 15:45:26 +0900

    Create GameScore

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class Scoreboard : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Transform container;
+    [SerializeField] private GameObject scoreboardItemPrefab;
+
+    Dictionary<Player, ScoreboardItem> scoreboardItems = new Dictionary<Player, ScoreboardItem>();
+
+    private void Start()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            AddSocreboardItem(player);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        AddSocreboardItem(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player ohterPlayer)
+    {
+        RemoveScoreboardItem(ohterPlayer);
+    }
+
+    private void AddSocreboardItem(Player player)
+    {
+        ScoreboardItem item = Instantiate(scoreboardItemPrefab, container).GetComponent<ScoreboardItem>();
+        item.Initialize(player);
+        scoreboardItems[player] = item;
+    }
+
+    private void RemoveScoreboardItem(Player player)
+    {
+        Destroy(scoreboardItems[player].gameObject);
+        scoreboardItems.Remove(player);
+    }
+}
